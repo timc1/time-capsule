02f0f38e9e36af7b9765cf9fce8c91225acfb475
tim <timothychang94@gmail.com>
2018-12-09 23:55:01 -0800

    update colors

--- src/components/questionnaire/steps/shared/index.js
+++ src/components/questionnaire/steps/shared/index.js
@@ -6,7 +6,7 @@ import { zoomIn, UnstyledButton } from '../../../shared/styles'
 const SmallModalContainer = styled.div`
   position: relative;
   margin: calc(25vh) auto 100px auto;
-  max-width: 340px;
+  max-width: 400px;
   width: 100%;
   animation: ${zoomIn} 0.15s ease-in;
 
@@ -27,16 +27,14 @@ const ClickForMoreButton = styled(UnstyledButton)`
   padding: 5px 10px;
   outline: none;
   cursor: pointer;
-  transition-property: opacity, transform;
-  transition: 0.1s ease-in;
   > span {
-    color: var(--white1);
+    color: var(--gray2);
     font-size: var(--fontsm);
     opacity: 0.7;
     transition: opacity 0.1s ease-in;
     &::before {
       border-radius: var(--baseborderradius);
-      box-shadow: 0 0 1px var(--black4);
+      box-shadow: 0 0 1px var(--gray2);
       transform: scale(0.8);
       opacity: 0;
       transition-property: opacity, transform;
@@ -58,7 +56,6 @@ const ClickForMoreButton = styled(UnstyledButton)`
   }
 
   &:active {
-    transform: translateY(1px);
     > span::before {
       opacity: 1;
       transform: scale(0.95);
@@ -74,7 +71,7 @@ const SectionName = React.memo(styled.h2`
   margin: 0 0 15px 0;
   font-size: var(--fontsm);
   font-weight: var(--fontbold);
-  color: var(--white);
+  color: var(--black);
   text-transform: uppercase;
 `)
 
