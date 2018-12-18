b6817dbbc290ba15cde38f65fe0729f9a68f6afe
tim <timothychang94@gmail.com>
2018-12-18 13:21:39 -0800

    Add community, moonshot, health tests

---
+++ src/components/questionnaire/__tests__/steps/bonus_community.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { render, fireEvent, wait } from 'react-testing-library'
+
+describe('Questionnaire -- Bonus Community', () => {
+  jest.doMock(`../../../shared/hooks/useQuestionnaire`, () => {
+    return () => window.getQuestionnaireContext('BONUS_COMMUNITY')
+  })
+
+  const Community = require('../../index').default
+
+  test('Should render a single textarea and a disabled next button.', async () => {
+    const { getByTestId } = render(<Community />)
+    const nextButton = getByTestId('next-button')
+
+    const textarea = getByTestId('textarea')
+    expect(textarea).toBeInTheDocument()
+    expect(nextButton).toHaveAttribute('disabled')
+
+    fireEvent.change(textarea, {
+      target: {
+        value: 'This is a test! 🤓',
+      },
+    })
+    await wait(() => {
+      expect(nextButton).not.toHaveAttribute('disabled')
+    })
+  })
+})
